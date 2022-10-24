52cadd58edd0ceef98b686d3100461a0551d3401
fajardd <fjrdwc@gmail.com>
2022-10-24 13:05:16 +0700

    update progres home

--- App.js
+++ App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TouchableOpacity, StatusBar, TextInput  } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import MenuBar from './src/components/MenuBar';
 
 const App = () => {
+
+  const [pencarian, setPencarian] = useState('');
+
   return (
-    <View style={{ flex:1,paddingTop:20,justifyContent:'center',padding:20,backgroundColor: '#FF8787' }}>
-      <Text style={Styles.text1}>Hello World</Text>
-      <Text style={Styles.text2}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</Text>
+    <View style={{flex: 1, backgroundColor:'#f4f4f4'}}>
+      <View style={{flex: 1,marginHorizontal:20, marginTop:10 }}>
+        <StatusBar barStyle="dark-content" backgroundColor={'#f4f4f4'} />
+        <Text style={{ color:'#212121' }}>Hello</Text>
+        <Text style={{ fontSize:22, fontWeight:'bold', color:'#212121' }}>Fajar Dwi 👋</Text>
+        <View style={{ flexDirection:'row' }}>
+          <TextInput 
+          value={pencarian}
+          onChangeText={text => setPencarian(text)}
+          placeholder="Cari informasi hewan" 
+          style={{ 
+            backgroundColor:'#FFFFFF', 
+            elevation:3, 
+            marginTop:20,
+            paddingLeft:10,
+            borderRadius:5,
+            flex:1,
+          }}
+          />
+          <TouchableOpacity
+          style={{ 
+            justifyContent:'center', 
+            alignItems:'center', 
+            backgroundColor:'#0082F7',
+            marginTop:20,
+            paddingHorizontal:20,
+            borderRadius:5,
+            marginLeft:10,
+            elevation:3,
+          }}>
+            <Icon name="search" size={25} color="#FFFFFF" />
+          </TouchableOpacity>
+        </View>
+      </View>
+      <MenuBar />
     </View>
   );
-};
+}
 
 export default App;
-
-const Styles = StyleSheet.create({
-  text1:{
-    fontSize:20,
-    textAlign:'center',
-    fontWeight: 'bold',
-    color:'white',
-  },
-  text2:{
-    textAlign:'justify',
-    color:'white',
-  },
-});
